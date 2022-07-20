48b042564d6fb1d5559c0c65335e288938e8b890
Michael Müller <michael.mueller@uos.de>
2022-07-20 17:50:56 +0200

    Added B-link tree benchmark.

---
+++ repos/mml/src/app/blinktree/target.mk
@@ -0,0 +1,15 @@
+TARGET = blinktree
+# soure file for benchmark framework
+SRC_MXBENCH = benchmark/workload_set.cpp
+SRC_MXBENCH += benchmark/workload.cpp
+SRC_MXBENCH += benchmark/cores.cpp
+SRC_MXBENCH += benchmark/perf.cpp
+SRC_MXBENCH += benchmark/string_util.cpp
+# source files for blinktree benchmark
+SRC_BTREE += blinktree_benchmark/main.cpp
+SRC_BTREE += blinktree_benchmark/benchmark.cpp
+
+SRC_CC = ${SRC_MXBENCH} ${SRC_BTREE}
+LIBS += base libc stdcxx mxtasking     
+CC_OPT += -Wno-error -fno-aligned-new
+CC_CXX_WARN_STRICT =
