8acd0741d41a338a0d1ef58626ad0ed590b02229
Michael Müller <michael.mueller@uos.de>
2022-07-21 12:40:55 +0200

    hello_mxtask: Store tasks in std::vector.

--- repos/mml/src/app/hello_mxtask/main.cc
+++ repos/mml/src/app/hello_mxtask/main.cc
@@ -61,7 +61,8 @@ void Libc::Component::construct(Libc::Env &env)
         Genode::log("Initialized system environment for MxTasking");
         Genode::log("Running on core ", mx::system::topology::core_id()); 
         const auto cores = mx::util::core_set::build(1);
-        
+
+        std::vector<mx::tasking::TaskInterface *> tasks;
 
         // Create a runtime for the given cores.
         mx::tasking::runtime_guard _{cores};
@@ -72,6 +73,7 @@ void Libc::Component::construct(Libc::Env &env)
         // Create an instance of the HelloWorldTask with the current core as first
         // parameter. The core is required for memory allocation.
         auto *hello_world_task = mx::tasking::runtime::new_task<HelloWorldTask>(cores.front());
+        tasks.emplace_back(hello_world_task);
 
         // Annotate the task to run on the first core.
         hello_world_task->annotate(cores.front());
@@ -79,11 +81,17 @@ void Libc::Component::construct(Libc::Env &env)
 
         auto *task2 = mx::tasking::runtime::new_task<HelloWorldTask>(cores.front());
         task2->annotate(cores.front());
+        tasks.emplace_back(task2);
 
         // Schedule the task.
         Genode::log("Spawning new task.");
-        mx::tasking::runtime::spawn(*hello_world_task);
-        mx::tasking::runtime::spawn(*task2);
+        //mx::tasking::runtime::spawn(*hello_world_task);
+        //mx::tasking::runtime::spawn(*task2);
+        
+        for (auto task : tasks) {
+            mx::tasking::runtime::spawn(*task);
+        }
+
         Genode::log("Task spawned.");
     });
 }
