410cec6aa3630d9636b59284ac3a0e7a9a7130d8
Marcel Lütke Dreimann <marcel.luetkedreimann@uos.de>
2022-08-29 16:03:46 +0200

    added bench info

--- repos/hello_gpgpu/src/hello_gpgpu/main.cc
+++ repos/hello_gpgpu/src/hello_gpgpu/main.cc
@@ -67,23 +67,23 @@ void testvm_construct(Genode::Env &env)
 		Genode::log("===Run gramschmidt===");
 		ns_gramschmidt::main(0, 0);
 		Genode::log("===Run lu===");
-		ns_lu::main(0, 0);
+		ns_lu::main(0, 0); // Non-Matching CPU-GPU Outputs Beyond Error Threshold of 0.05 Percent: 127
 
 		Genode::log("===Run correlation===");
-		ns_correlation::main(0, 0);
+		ns_correlation::main(0, 0); // Non-Matching CPU-GPU Outputs Beyond Error Threshold of 1.05 Percent: 4188163
 		Genode::log("===Run covariance===");
 		ns_covariance::main(0, 0);
 
 		Genode::log("===Run adi===");
-		ns_adi::main(0, 0);
+		ns_adi::main(0, 0); // CPU-GPU Outputs Beyond Error Threshold of 10.05 Percent: 455
 		Genode::log("===Run convolution_2d===");
 		ns_convolution_2d::main(0, 0);
-		Genode::log("===Run convolution_3d===");
-		ns_convolution_3d::main(0, 0);
+		Genode::log("===Run convolution_3d==="); // this one may also destroy other benchmarks!
+		ns_convolution_3d::main(0, 0); // Non-Matching CPU-GPU Outputs Beyond Error Threshold of 1.05 Percent: 234484
 		Genode::log("===Run fdtd_2d===");
 		ns_fdtd_2d::main(0, 0);
 		Genode::log("===Run jacobi_1d_imper===");
-		ns_jacobi_1d_imper::main(0, 0);
+		ns_jacobi_1d_imper::main(0, 0); // Non-Matching CPU-GPU Outputs Beyond Error Threshold of 10.05 Percent: 245
 		Genode::log("===Run jacobi_2d_imper===");
 		ns_jacobi_2d_imper::main(0, 0);*/
 	});
