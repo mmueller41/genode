d3377454d63095a71bc74bf2a293733f4685ffc6
Michael Mueller <michael.mueller@uos.de>
2024-10-24 14:25:25 +0200

    Changed compilation flags for blinktree.

--- repos/mml/src/app/blinktree/target.mk
+++ repos/mml/src/app/blinktree/target.mk
@@ -17,8 +17,8 @@ LIBS += base libc stdcxx mxtasking
 EXT_OBJECTS += /usr/local/genode/tool/lib/clang/14.0.5/lib/linux/libclang_rt.builtins-x86_64.a /usr/local/genode/tool/lib/libatomic.a 
 CUSTOM_CC = /usr/local/genode/tool/bin/clang
 CUSTOM_CXX = /usr/local/genode/tool/bin/clang++
-CC_OPT += --target=x86_64-genode --sysroot=/does/not/exist --gcc-toolchain=$(GENODE_GCC_TOOLCHAIN_DIR) -Wno-error -O2 -g -fno-aligned-new -DNDEBUG -I$(MXINC_DIR) -std=c++17 #-D_GLIBCXX_ATOMIC_BUILTINS_8 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
-CC_OPT +=  -femulated-tls -DCLANG_CXX11_ATOMICS
+CC_OPT += --target=x86_64-genode --sysroot=/does/not/exist --gcc-toolchain=$(GENODE_GCC_TOOLCHAIN_DIR) -Wno-error -O2 -g -DNDEBUG -I$(MXINC_DIR) -std=c++20 #-D_GLIBCXX_ATOMIC_BUILTINS_8 -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+#CC_OPT +=  -femulated-tls -DCLANG_CXX11_ATOMICS
 CC_CXX_WARN_STRICT =
 CUSTOM_CXX_LIB := $(CROSS_DEV_PREFIX)g++
 #CXX_LD += $(CROSS_DEV_PREFIX)g++ 
