b4e45b87e94fbc4ccc506f81dc1a34e0c222fb11
Michael Mueller <michael.mueller@uos.de>
2025-01-28 13:44:19 +0100

    Adjusted options for lwIP.

--- repos/libports/src/lib/lwip/include/lwipopts.h
+++ repos/libports/src/lib/lwip/include/lwipopts.h
@@ -33,21 +33,21 @@ extern "C" {
 #define LWIP_DNS                    1  /* DNS support */
 #define LWIP_DHCP                   1  /* DHCP support */
 #define LWIP_SOCKET                 0  /* LwIP socket API */
-#define LWIP_NETIF_LOOPBACK         1  /* Looping back to same address? */
+#define LWIP_NETIF_LOOPBACK         0  /* Looping back to same address? */
 #define LWIP_STATS                  0  /* disable stating */
-#define LWIP_TCP_TIMESTAMPS         1
-#define TCP_LISTEN_BACKLOG          1
+#define LWIP_TCP_TIMESTAMPS         0
+#define TCP_LISTEN_BACKLOG          255
 #define TCP_MSS                     1460
 #define TCP_WND                     (80 * TCP_MSS)
 #define TCP_SND_BUF                 (80 * TCP_MSS)
 #define LWIP_WND_SCALE              3
 #define TCP_RCV_SCALE               2
-#define TCP_SND_QUEUELEN            ((8 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS))
+#define TCP_SND_QUEUELEN            ((512 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS))
 
 #define LWIP_NETIF_STATUS_CALLBACK  1  /* callback function used for interface changes */
 #define LWIP_NETIF_LINK_CALLBACK    1  /* callback function used for link-state changes */
 
-
+#define TCP_QUEUE_OOSEQ             1
 /***********************************
  ** Checksum calculation settings **
  ***********************************/
@@ -69,10 +69,10 @@ extern "C" {
 
 #define RECV_BUFSIZE_DEFAULT        (512*1024)
 
-#define PBUF_POOL_SIZE             96
+#define PBUF_POOL_SIZE             8192
 
-#define MEMP_NUM_SYS_TIMEOUT        16
-#define MEMP_NUM_TCP_PCB           128
+#define MEMP_NUM_SYS_TIMEOUT        64
+#define MEMP_NUM_TCP_PCB           1024
 
 #ifndef MEMCPY
 #define MEMCPY(dst,src,len)             genode_memcpy(dst,src,len)
