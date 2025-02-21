1b4f291be8d3bfbccfe995692343aa22f2854374
Michael Mueller <michael.mueller@uos.de>
2025-02-21 15:40:05 +0100

    Some test to check wether a user-space cell can access the topology model and its cell infopage.

--- repos/ealanos/src/app/volatile_cell/main.cc
+++ repos/ealanos/src/app/volatile_cell/main.cc
@@ -2,6 +2,7 @@
 #include <base/env.h>
 #include <base/log.h>
 #include <timer_session/connection.h>
+#include <tukija/syscalls.h>
 
 namespace Hoitaja_test
 {
@@ -18,9 +19,16 @@ class Hoitaja_test::Empty_cell
         Empty_cell(Genode::Env &env) : _env(env)
         {
             Genode::log("Volatile dummy cell started.");
+            Tukija::alloc();
+            Genode::log("Found topology model of size ", Tukija::Tip::tip()->length, " mapped at ", static_cast<const void *>(Tukija::Tip::tip()));
+            Tukija::Cip *cip = Tukija::Cip::cip();
+            Genode::log("CIP value: ", *reinterpret_cast<unsigned long*>(cip));
+            long pd_sel = _env.pd().native_pd().local_name();
+            Genode::log("pd_sel = ", pd_sel);
             _timer.msleep(10000);
             Genode::log("Exiting..");
             _env.parent().exit(0);
+
         }
 };
 
