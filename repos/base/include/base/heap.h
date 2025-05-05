16bec7c880924ac6dddb3feb4c211eebaf1a342a
Michael Mueller <michael.mueller@uos.de>
2025-05-05 18:11:26 +0200

    base: Fixed compiler warning.

--- repos/base/include/base/heap.h
+++ repos/base/include/base/heap.h
@@ -119,7 +119,7 @@ class Genode::Heap : public Allocator
 
 	public:
 
-		static constexpr size_t UNLIMITED = ~0;
+		static constexpr size_t UNLIMITED = ~0UL;
 
 		Heap(Ram_allocator *ram_allocator,
 		     Region_map    *region_map,
